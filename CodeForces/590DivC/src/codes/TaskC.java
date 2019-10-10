22763b62cf52c365d91a0317f33f80b7d47b1dc6
Surya Singh <singhsurya1012@gmail.com>
2019-10-11 04:20:29 +0530

    Codeforces 590 DivC solution

---
+++ CodeForces/590DivC/src/codes/TaskC.java
@@ -0,0 +1,9 @@
+package codes;
+
+import FastIO.InputReader;
+import FastIO.OutputWriter;
+
+public class TaskC {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+    }
+}
---
+++ CodeForces/590DivC/src/codes/TaskE.java
@@ -0,0 +1,9 @@
+package codes;
+
+import FastIO.InputReader;
+import FastIO.OutputWriter;
+
+public class TaskE {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+    }
+}
