abbd4dd47d1c0a00fed3ee408c28addde33a4a6f
Surya Singh <singhsurya1012@gmail.com>
2021-06-08 18:30:39 +0530

    Leetcode 8th june problem

--- LeetCode/src/medium/ConstructBinaryTreeFromInorderAndPreorderTraversal.java
+++ LeetCode/src/medium/ConstructBinaryTreeFromInorderAndPreorderTraversal.java
@@ -1,6 +1,8 @@
 package medium;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConstructBinaryTreeFromInorderAndPreorderTraversal {
 
@@ -23,8 +25,36 @@ public class ConstructBinaryTreeFromInorderAndPreorderTraversal {
         }
     }
 
-
+    Map<Integer,Integer> inOrderIndexMap = new HashMap<>();
+    int preOrderIndex;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        preOrderIndex = 0;
+
+        for(int i=0;i<inorder.length;i++){
+            inOrderIndexMap.put(inorder[i],i);
+        }
+
+        return constructTree(preorder,0, inorder.length-1);
+    }
+
+
+    public TreeNode constructTree(int[] preOrder, int left, int right){
+
+        if(left>right)
+            return null;
+
+
+        int val = preOrder[preOrderIndex++];
+        TreeNode node = new TreeNode(val);
+
+        node.left = constructTree(preOrder, left,inOrderIndexMap.get(val)-1);
+        node.right = constructTree(preOrder,inOrderIndexMap.get(val)+1,right);
+
+        return node;
+    }
+
+    public TreeNode buildTreeSlow(int[] preorder, int[] inorder) {
         int N = inorder.length;
 
         if (N == 0) {
@@ -46,10 +76,10 @@ public class ConstructBinaryTreeFromInorderAndPreorderTraversal {
         }
 
 
-        root.left = buildTree(Arrays.copyOfRange(preorder, 1, L + 1),
+        root.left = buildTreeSlow(Arrays.copyOfRange(preorder, 1, L + 1),
                 Arrays.copyOfRange(inorder, 0, L));
 
-        root.right = buildTree(Arrays.copyOfRange(preorder, L + 1, N),
+        root.right = buildTreeSlow(Arrays.copyOfRange(preorder, L + 1, N),
                 Arrays.copyOfRange(inorder, L + 1, N));
 
 
