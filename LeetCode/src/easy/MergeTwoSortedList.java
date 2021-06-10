ff8494be4b87310895dadd12267efa90034bd21c
Surya Singh <singhsurya1012@gmail.com>
2021-06-11 02:37:24 +0530

    Leetcode practise problems

--- LeetCode/src/easy/MergeTwoSortedList.java
+++ LeetCode/src/easy/MergeTwoSortedList.java
@@ -153,4 +153,39 @@ public class MergeTwoSortedList {
 
         return l1;
     }
+
+
+    public ListNode mergeTwoListsCleanSolution(ListNode l1, ListNode l2) {
+
+        if(l1==null)
+            return l2;
+
+        if(l2==null)
+            return l1;
+
+        if(l2.val<l1.val){
+            ListNode temp = l1;
+            l1= l2;
+            l2= temp;
+        }
+
+        ListNode head = l1;
+
+        while(l1!=null && l2!=null){
+
+            ListNode temp = null;
+            while(l1!=null && l1.val<=l2.val){
+                temp = l1;
+                l1 = l1.next;
+            }
+
+            temp.next = l2;
+
+            ListNode t = l1;
+            l1 = l2;
+            l2 = t;
+        }
+
+        return head;
+    }
 }
