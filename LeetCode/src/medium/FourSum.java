eb95ccde89ab404c0f0ee8a40b49ee2ba2662ebc
Surya Singh <singhsurya1012@gmail.com>
2021-07-16 16:40:37 +0530

    LC 16ht july

--- LeetCode/src/medium/FourSum.java
+++ LeetCode/src/medium/FourSum.java
@@ -2,9 +2,84 @@ package medium;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class FourSum {
+
+    public List<List<Integer>> fourSumGeneric(int[] nums, int target) {
+
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        return kSum(nums, 4, 0, target);
+    }
+
+    public List<List<Integer>> kSum(int[] nums, int k , int start, int target){
+        List<List<Integer>> result = new LinkedList<>();
+        int n = nums.length;
+
+        if(start==nums.length)
+            return result;
+
+        if(k==2){
+            return twoSum(nums, start, target);
+        }
+
+
+
+        for(int i=start;i<=n-k; i++){
+
+            if(i!=start && nums[i]==nums[i-1])
+                continue;
+
+            for(List<Integer> l: kSum(nums,k-1,i+1,target-nums[i])){
+                l.add(nums[i]);
+                result.add(l);
+            }
+        }
+
+        return result;
+    }
+
+    public List<List<Integer>> twoSum(int[] nums, int start, int target){
+
+        List<List<Integer>> result = new LinkedList<>();
+
+        int l=start;
+        int r = nums.length-1;
+
+        while(l<r) {
+
+            int sum = nums[l] + nums[r];
+
+            if(sum==target){
+
+                List<Integer> ans= new LinkedList<>();
+                ans.add(nums[l]);
+                ans.add(nums[r]);
+                result.add(ans);
+
+                l++;
+                r--;
+
+                while(l<=r && nums[l]==nums[l-1]){
+                    l++;
+                }
+
+                while(r>=l && nums[r]==nums[r+1]){
+                    r--;
+                }
+
+            }else if(sum<target){
+                l++;
+            }else {
+                r--;
+            }
+        }
+        return result;
+    }
+
 
     //Time Complexity = O(N^3)
     public List<List<Integer>> fourSum(int[] nums, int target) {
