08c78eab61c56273df546ed785b85d61ac975dba
Surya Singh <singhsurya1012@gmail.com>
2021-07-19 04:12:45 +0530

    LC Practise

---
+++ LeetCode/src/easy/BuildArrayFromPermutation.java
@@ -0,0 +1,15 @@
+package easy;
+
+public class BuildArrayFromPermutation {
+
+    public int[] buildArray(int[] nums) {
+
+        int n =nums.length;
+        int[] ans = new int[n];
+
+        for(int i=0; i<n; i++){
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
